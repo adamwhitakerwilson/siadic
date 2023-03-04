522318be26409c53a13f7b46dbb68d85a8398924
Adam <adamwhitakerwilson@gmail.com>
2023-03-04 13:38:45 -0500

    Add serv.js

--- serv.js
+++ serv.js
@@ -1,18 +1,20 @@
-// content of index.js
-const http = require('http')
-const port = 3000
+const express = require('express'); //Import the express dependency
+const path = require('path');
+const app = express();              //Instantiate an express app, the main work horse of this server
+const port = 3000;                  //Save the port number where your server will be listening
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+//app.use(express.static('static'));
+//app.use('/static', express.static(path.join(__dirname, 'images')))
+app.use(express.static(path.join(__dirname, 'static'), {index: false}));
+app.use(express.static(path.join(__dirname, 'static/images'), {index: false}));
+app.use(express.static(path.join(__dirname, 'images'), {index: false}));
+//Idiomatic expression in express to route and respond to a client request
+app.get('/', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile(path.resolve('./index.html'));      //server responds by sending the index.html file to the client's browser
+                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+});
 
-const server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`); 
+});
